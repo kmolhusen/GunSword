f206529b65f04637ca8bfadf6ba7986028052949
kmolhusen <kristopher_molhusen05@yahoo.com>
2016-05-13 20:57:30 -0500

    new program MyPomodoro
    timer which counts to 25 minutes

---
+++ MyPomodoro/MyPomodoro/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace MyPomodoro
+{
+    partial class frmPomodoro
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPomodoro));
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Stencil", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(94, 9);
+            this.lblDisplay.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(0, 38);
+            this.lblDisplay.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.FlatAppearance.BorderColor = System.Drawing.SystemColors.ControlLight;
+            this.btnStart.Font = new System.Drawing.Font("Stencil", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnStart.Image = ((System.Drawing.Image)(resources.GetObject("btnStart.Image")));
+            this.btnStart.Location = new System.Drawing.Point(14, 66);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(263, 196);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Pomodoro Time!";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // frmPomodoro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(291, 275);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblDisplay);
+            this.Font = new System.Drawing.Font("Stencil", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "frmPomodoro";
+            this.Text = "MyPomodoro!";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDisplay;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+
+    }
+}
+
---
+++ MyPomodoro/MyPomodoro/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MyPomodoro
+{
+    public partial class frmPomodoro : Form
+    {
+        public frmPomodoro()
+        {
+            InitializeComponent();
+
+        }
+
+        int min, sec, ms = 0;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            
+                timer1.Start();
+            
+
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblDisplay.Text = min + ":" + sec + ":" + ms;
+            ms++;
+            if (ms > 9)
+            {
+                sec++;
+                ms = 0;
+                    if (sec > 59)
+                    {
+                        min++;
+                        sec = 0;
+                    }
+            }
+            if (min == 25)
+            {
+                timer1.Stop();
+
+                for (int i = 0; i < 20; i++)
+                {
+                    lblDisplay.Hide();
+                    lblDisplay.Show();
+                }
+            }
+
+
+        }
+    }
+}
